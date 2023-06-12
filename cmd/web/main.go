199dcf8e8e78fc56712ecb7b0e6813d22652e8e7
nurlykhan <nurlykhan.salamatuly@samgau.com>
2023-06-12 09:43:29 +0600

    feat(): init routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -14,9 +14,7 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
-
 	flag.Parse()
-
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
@@ -25,13 +23,24 @@ func main() {
 		infoLog:  infoLog,
 	}
 
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  app.routes(),
+		Handler:  mux,
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
+
 	err := srv.ListenAndServe()
+
 	errorLog.Fatal(err)
 }
